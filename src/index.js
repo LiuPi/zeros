84157d9a29fe995094f37eb062f196d71b5ca96a
Liudmila <mizuxira@yandex.ru>
2018-03-01 23:20:57 +0700

    zeros/feature: implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+function factorTwo(n) {
+    let f2 = 0;
+    for (let d = 2; d <= n; d *= 2)
+        f2 += Math.floor(n/d);
+    return f2;
 }
+function factorFive(n) {
+    let f5 = 0;
+    for (let d = 5; d <= n; d *= 5)
+        f5 += Math.floor(n/d);
+    return f5;
+}
+
+module.exports = function getZerosCount(number) {
+    let factorsTwo = 0;
+    let factorsFive = 0;
+    let n = number;
+    factorsTwo += factorTwo(n);
+    factorsFive += factorFive(n);
+    return Math.min(factorsTwo,factorsFive);
+
+};
